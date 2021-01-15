5bc44ed85d6f2623317886e0a76dfeb96298f3f2
jirotakeo <jirotakeo@gmail.com>
2021-01-15 20:18:13 +0300

    first commit

---
+++ src/hr_service/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+
+
+def home(request):
+    date = datetime.datetime.now().date()
+    name = 'jiro'
+    context = {'name': name, 'date': date}
+    return render(request, 'home.html', context)
