c5036c53a38d1171df335562116ae0f98d9323a5
jirotakeo <jirotakeo@gmail.com>
2021-03-01 19:09:45 +0300

    добавил файлы в git

---
+++ src/scraping/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import City, Language, Vacancy, Error, Url
+
+
+# Register your models here.
+class CityAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class LanguageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(City, CityAdmin)
+admin.site.register(Language, LanguageAdmin)
+admin.site.register(Vacancy)
+admin.site.register(Error)
+admin.site.register(Url)
---
+++ src/scraping/models.py
@@ -0,0 +1,67 @@
+import jsonfield
+from django.db import models
+
+
+
+def default_urls():
+    return {'work': '', 'rabota': ''}
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Город', unique=True)
+    slug = models.SlugField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
+    slug = models.SlugField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='Язык программирования')
+    timestamp = models.DateField(auto_now_add=True)
+
+
+    class Meta:
+        verbose_name = 'Вакансию'
+        verbose_name_plural = 'Вакансии'
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return self.title
+
+
+class Error(models.Model):
+    timestamp = models.DateField(auto_now_add=True)
+    data = jsonfield.JSONField()
+
+    def __str__(self):
+        return str(self.timestamp)
+
+
+class Url(models.Model):
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='Язык программирования')
+    url_data = jsonfield.JSONField(default=default_urls)
+
+    class Meta:
+        unique_together = ('city', 'language')
